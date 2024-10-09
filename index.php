d4b62d42fd2c003cffc09fc465c207a33d3f3856
Victor <vmarmar1808@g.educaand.es>
2024-10-09 12:25:17 +0200

    Comienzo del proyecto

---
+++ index.php
@@ -0,0 +1,13 @@
+/<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>el calaita</title>
+</head>
+<body>
+
+<?php echo "<h1> hello world </h1>"?>
+
+</body>
+</html>
